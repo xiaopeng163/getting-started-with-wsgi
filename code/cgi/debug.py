4801648557da71d93c939a0a233cb48b320b54b1
Peng Xiao <xiaoquwl@gmail.com>
2015-01-08 15:24:46 +0800

    update summary.md

--- code/cgi/debug.py
+++ code/cgi/debug.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-# Copyright 2014 Cisco Systems, Inc.
-# All rights reserved.
-
-""" """
+import cgitb
+print "Content-Type: text/html"
+print
+try:
+    print """\
+<html>
+<body>
+<h2>Hello World! %s </h2>
+</body>
+</html>
+""" % str(1/0)
+except:
+    cgitb.handler()
